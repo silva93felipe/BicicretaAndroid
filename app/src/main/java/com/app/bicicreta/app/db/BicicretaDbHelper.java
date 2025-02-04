f01bd2d940134f30ce371c0b824d39ea403635e6
Felipe Santos <silva93felipe@gmail.com>
2025-02-04 17:58:01 -0300

    feat: Criando script para tabelas viagem, bicicleta e peça

--- app/src/main/java/com/app/bicicreta/app/db/BicicretaDbHelper.java
+++ app/src/main/java/com/app/bicicreta/app/db/BicicretaDbHelper.java
@@ -7,25 +7,25 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class BicicretaDbHelper extends SQLiteOpenHelper {
-    private Context context;
     private static final String DATABASE_NAME = "bicreta.db";
     private static final int DATABASE_VERSION = 1;
     public BicicretaDbHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        this.context = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(sqlCreateTableUser());
+        db.execSQL(sqlCreteTables());
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {}
 
-    }
-
-    private String sqlCreateTableUser(){
-        return "CREATE TABLE user ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT );";
+    private String sqlCreteTables(){
+        String sql = "CREATE TABLE bicicleta ( id INTEGER PRIMARY KEY AUTOINCREMENT, descricao VARCHAR(40), valor REAL DEFAULT 0, quilometros_rodados INTEGER DEFAULT 0 ); ";
+        sql += "CREATE TABLE peca ( id INTEGER PRIMARY KEY AUTOINCREMENT, descricao VARCHAR(50), valor REAL DEFAULT 0.0, quilometros_rodados INTEGER DEFAULT 0, data_compra DEFAULT CURRENT_TIMESTAMP, bike_id INTEGER, FOREIGN KEY (bike_id) REFERENCES bicicleta (id) ); ";
+        sql += "CREATE TABLE viagem ( id INTEGER PRIMARY KEY AUTOINCREMENT, local VARCHAR(40), quilometros_rodados INTEGER DEFAULT 0, data_viagem DEFAULT CURRENT_TIMESTAMP, bike_id INTEGER, FOREIGN KEY (bike_id) REFERENCES bicicleta (id) ); ";
+        sql += "CREATE TABLE user ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT ); ";
+        return sql;
     }
 }
