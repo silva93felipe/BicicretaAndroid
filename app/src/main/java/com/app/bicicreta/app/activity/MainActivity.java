a978930d2e58682275e92bbdec237bc274996fef
Felipe Santos <silva93felipe@gmail.com>
2025-02-11 16:44:28 -0300

    feat: Implementando grafico de viagens por mes

--- app/src/main/java/com/app/bicicreta/app/activity/MainActivity.java
+++ app/src/main/java/com/app/bicicreta/app/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.app.bicicreta.app.activity;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -16,12 +17,24 @@ import com.app.bicicreta.app.model.Viagem;
 import com.app.bicicreta.app.repository.PecaRepository;
 import com.app.bicicreta.app.repository.UserRepository;
 import com.app.bicicreta.app.repository.ViagemRepository;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     TextView nomeUsarioTextView, quilomentrosRodadosTextView, destinoUltimaViagem,
             dataUltimaViagem, quilometroUltimaViagem, descricaoPecaUltimaCompra, dataUltimaCompra, quilometrosUltimaCompra;
     RecyclerView ultimasViagensRecyclerView;
     ImageView mapTabImagemView, toolTabImagemView, bicicletaTabImagemView;
+    BarChart viagemBarChart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,9 +67,36 @@ public class MainActivity extends AppCompatActivity {
         descricaoPecaUltimaCompra = findViewById(R.id.descricaoPecaUltimaCompraTextView);
         dataUltimaCompra = findViewById(R.id.dataUltimaCompraTextView);
         quilometrosUltimaCompra = findViewById(R.id.quilometroUltimaPecaTextView);
-       // ultimasViagensRecyclerView = findViewById(R.id.ultimasViagensRecyclerView);
-       // ultimasViagensRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        //ultimasViagensRecyclerView.setHasFixedSize(true);
+        viagemBarChart = findViewById(R.id.chartViagensPorMes);
+        viagemBarChart.getAxisRight().setDrawLabels(false);
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0 , 45f));
+        entries.add(new BarEntry(1 , 80f));
+        entries.add(new BarEntry(2 , 65f));
+        entries.add(new BarEntry(3 , 38f));
+
+        YAxis yAxis = viagemBarChart.getAxisLeft();
+        yAxis.setAxisMinimum(0f);
+        yAxis.setAxisMaximum(100f);
+        yAxis.setAxisLineWidth(2f);
+        yAxis.setAxisLineColor(Color.BLACK);
+        yAxis.setLabelCount(10);
+
+        BarDataSet dataSet = new BarDataSet(entries, "Sub");
+        dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+
+        BarData barData = new BarData(dataSet);
+        viagemBarChart.setData(barData);
+
+        viagemBarChart.getDescription().setEnabled(false);
+        viagemBarChart.invalidate();
+
+        viagemBarChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(Arrays.asList("Maths", "Science", "English", "IT")));
+        viagemBarChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        viagemBarChart.getXAxis().setGranularity(1f);
+        viagemBarChart.getXAxis().setGranularityEnabled(true);
+
+
     }
 
     private void handleNavigation(Class activity){
